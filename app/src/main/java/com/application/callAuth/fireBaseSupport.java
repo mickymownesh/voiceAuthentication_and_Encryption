88cfd34a0a7920417e8ec244cd10100133607337
Mownesh <mickymownesh@gmail.com>
2020-10-26 15:13:00 +0530

    successfully tested the firebase connection and the conection integrity also tested the real time database of firebase

---
+++ app/src/main/java/com/application/callAuth/fireBaseSupport.java
@@ -0,0 +1,17 @@
+package com.application.callAuth;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class fireBaseSupport {
+
+    private FirebaseDatabase masterDB = FirebaseDatabase.getInstance();
+    private DatabaseReference masterDBRefrence = masterDB.getReference();
+
+    fireBaseSupport(){
+
+    masterDBRefrence = masterDB.getReference().child("user");
+    masterDBRefrence.setValue("test");
+
+    }
+}
